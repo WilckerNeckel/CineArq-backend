a31279afd32684d247a1f1b6a8d7a6652cdaa0d3
Wilcker Neckel <wilckerwrsn@gmail.com>
2024-04-29 22:29:04 -0400

    adding statics configuration on settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,4 +147,9 @@ CORS_ORIGIN_WHITELIST = [
    '52.203.193.235'
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
