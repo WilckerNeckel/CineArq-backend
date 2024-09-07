f2300f9a411dbf850cf06960b2f7cda5f0d0b034
Wilcker Neckel <wilckerwrsn@gmail.com>
2024-09-07 17:06:06 -0400

    f

--- formulario/models.py
+++ formulario/models.py
@@ -6,8 +6,8 @@ class Formularios(models.Model):
     email = models.EmailField()
     telefone = models.CharField(max_length=14)
     codigo = models.CharField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         db_table = 'formularios'
